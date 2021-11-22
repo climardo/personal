bb8c5a630f1bd5d49128dffbf169ead8ee33e3c3
Carlos Limardo <climardo@gmail.com>
2021-11-22 17:14:36 -0500

    clean up scripts and add comments

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,56 +1,62 @@
-let checkboxes = [
-    "bitcoin",
-    "ethereum",
-    "analysis",
-    "learn",
-    "news",
-    "official",
-    "tools"
-]
+// Used in applyOptions() to determine which checkboxes are checked
+let options = document.getElementsByClassName("form-check-input")
 
-let allItems = []
-for (let j = 0; j < checkboxes.length; j++) {
-    let checkbox = checkboxes[j]
-    allItems[checkbox] = document.getElementsByClassName(checkbox)
-    /* allItems.push({[checkbox]: document.getElementsByClassName(checkbox)}) */
+// Set value 
+let checkboxValues = []
+for (let i = 0; i < options.length; i++) {
+    // Watch to the checkboxes and call applyOptions() when detected
+    options[i].addEventListener('change', applyOptions)
+
+    // Add text values of each option to array checkboxValues
+    checkboxValues[i] = options[i].value
 }
 
-function applyOptions() {
+// Array containing all items on the page organized by tags in the next for() statement.
+// allItems["bitcoin"] is an array of elements on the page with a "bitcoin" tag
+let allItems = []
+for (let i = 0; i < checkboxValues.length; i++) {
+    let checkbox = checkboxValues[i]
+    allItems[checkbox] = document.getElementsByClassName(checkbox)
+}
 
-    function addOrRemove(array, value) {
-        var index = array.indexOf(value);
+// A function to remove duplicates from an array
+function addOrRemove(array, value) {
+    var index = array.indexOf(value);
 
-        if (index === -1) {
-            array.push(value);
-        } else {
-            array.splice(index, 1);
-        }
+    if (index === -1) {
+        array.push(value);
+    } else {
+        array.splice(index, 1);
     }
-    let uncheckedOptions = []
+}
+
+// A function to shows/hide elements based on the state of matching checkboxValues
+function applyOptions() {
+    let uncheckedOptions = [] // Initialize array
+    let checkedOptions = [] // Initialize array
+
+    // Add unchecked options to the array
     for (let i = 0; i < options.length; i++) {
-        if (options[i].checked == false) {
+        if (options[i].checked == true) {
+            addOrRemove(checkedOptions, options[i].value)
+        } else {
             addOrRemove(uncheckedOptions, options[i].value)
         }
     }
 
-    for (let k = 0; k < uncheckedOptions.length; k++) {
-        let unchecked = uncheckedOptions[k]
-        for (let l = 0; l < allItems[unchecked].length; l++) {
-            allItems[unchecked][l].classList.add("d-none")
+    // Hide unchecked options by adding d-none class
+    for (let i = 0; i < uncheckedOptions.length; i++) {
+        let unchecked = uncheckedOptions[i]
+        for (let j = 0; j < allItems[unchecked].length; j++) {
+            allItems[unchecked][j].classList.add("d-none")
         }
     }
 
-    let checkedOptions = checkboxes.filter(x => !uncheckedOptions.includes(x))
-
-    for (let k = 0; k < checkedOptions.length; k++) {
-        let checked = checkedOptions[k]
-        for (let l = 0; l < allItems[checked].length; l++) {
-            allItems[checked][l].classList.remove("d-none")
+    // Show checked options by removing d-none class
+    for (let i = 0; i < checkedOptions.length; i++) {
+        let checked = checkedOptions[i]
+        for (let j = 0; j < allItems[checked].length; j++) {
+            allItems[checked][j].classList.remove("d-none")
         }
     }
-    console.log(checkedOptions)
-
 }
-
-let options = document.getElementsByClassName("form-check-input")
-document.addEventListener('change', applyOptions);
